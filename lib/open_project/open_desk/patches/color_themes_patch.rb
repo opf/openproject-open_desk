0ac75a5250877792ccfb6ad08f908b0ce914c0f7
Oliver Günther <mail@oliverguenther.de>
2024-03-08 20:51:32 +0100

    Update open_desk theme (#6)
    * Update open_desk theme
    
    * Update lib/open_project/open_desk/patches/color_themes_patch.rb
    
    Co-authored-by: Henriette Darge <h.darge@openproject.com>
    
    ---------
    
    Co-authored-by: Henriette Darge <h.darge@openproject.com>

--- lib/open_project/open_desk/patches/color_themes_patch.rb
+++ lib/open_project/open_desk/patches/color_themes_patch.rb
@@ -42,10 +42,8 @@ module OpenProject::OpenDesk::Patches
         {
           theme: OpenProject::CustomStyles::ColorThemes::OPEN_DESK_THEME_NAME,
           colors: {
-            "primary-color": "#5E27DD",
-            "primary-color-dark": "#4526AA",
-            "alternative-color": "#5E27DD",
-            "content-link-color": "#5E27DD",
+            "primary-button-color": "#5E27DD",
+            "accent-color": "#5E27DD",
             "header-bg-color": "#FFFFFF",
             "header-item-bg-hover-color": "#E5E5E5",
             "header-item-font-color": "#000000",
