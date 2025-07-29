2222ab15edc9202c816bb2b7c53c6107fb871785
Behrokh Satarnejad <62008897+bsatarnejad@users.noreply.github.com>
2025-07-29 10:47:31 +0200

    [65936] Adapt openDesk waffle menu to look like in OpenProject. (#11)
    * load waffle menu in a dialog instead of a dropdown
    
    * show logo in header
    
    * remove padding left on the items
    
    * turbo render menu instead of page reload
    
    * we should show the waffle menu with global items when the user is not logged in
    
    * remove find_navigational_items from the helper
    
    * remove a deprecated class and remove styles on elemnt
    
    * remove primer classes
    
    * Adapt test to new modules button
    
    ---------
    
    Co-authored-by: Henriette Darge <h.darge@openproject.com>

--- lib/open_project/open_desk/patches/top_menu_helper_patch.rb
+++ lib/open_project/open_desk/patches/top_menu_helper_patch.rb
@@ -35,13 +35,27 @@ module OpenProject::OpenDesk::Patches
     module InstanceMethods
       def render_module_top_menu_node(*)
         items = first_level_menu_items_for(:open_desk_menu)
-        unless items.empty?
-          render_menu_dropdown_with_items(
-            label: "",
-            label_options: { icon: "icon-menu", title: I18n.t("open_desk.central_navigation_menu") },
-            items:,
-            options: { drop_down_id: "more-menu", drop_down_class: "drop-down--modules ", menu_item_class: "hidden-for-mobile" }
-          )
+        return if items.empty?
+
+        render Primer::Alpha::Dialog.new(classes: "op-app-menu--item",
+                                         title: I18n.t("open_desk.central_navigation_menu"),
+                                         size: :small,
+                                         menu_id: "op-app-header--modules-menu",
+                                         position: :left) do |dialog|
+          dialog.with_show_button(icon: "op-grid-menu",
+                                  scheme: :invisible,
+                                  classes: "op-app-header--primer-button",
+                                  test_selector: "op-app-header--modules-menu-button",
+                                  "aria-controls": "op-app-header--modules-menu-list",
+                                  "aria-label": I18n.t("open_desk.central_navigation_menu"))
+          dialog.with_header(classes: "op-app-header--modules-menu-header") do
+            render_logo_icon
+          end
+          dialog.with_body do
+            turbo_frame_tag "opendesk-central-navigation",
+                            src: open_desk_menu_path,
+                            loading: :lazy
+          end
         end
       end
     end
